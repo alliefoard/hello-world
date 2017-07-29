649a5639ce7562012df6555a4198b7ee6ee2832c
Allie Foard <alliefoard@gmail.com>
2017-07-29 15:18:39 -0400

    Added More Design Elements to Headers

---
+++ anissa-child/sidebar.php
@@ -0,0 +1,16 @@
+<!-- Commenting out sidebar 
+
+<?php
+/**
+ * The sidebar containing the main widget areas.
+ *
+ * @package anissa
+ */
+?>
+
+<div id="secondary" class="widget-area sidebar" role="complementary">
+  <?php dynamic_sidebar( 'sidebar-1' ); ?>
+</div>
+<!-- #secondary -->
+
+<!--
